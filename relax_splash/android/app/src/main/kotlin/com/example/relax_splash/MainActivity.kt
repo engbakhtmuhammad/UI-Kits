3dd0838ab61fca332f34db7390d2ea81bbc664fe
bakhtkhilji23 <bakhtkhilji23@gmail.com>
2023-02-17 14:59:55 +0500

    splash screen

---
+++ relax_splash/android/app/src/main/kotlin/com/example/relax_splash/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.relax_splash
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ relax_splash/lib/splash screen/relax_splash.dart
@@ -0,0 +1,118 @@
+import 'package:relax_splash/splash%20screen/components/care_view.dart';
+import 'package:relax_splash/splash%20screen/components/center_next_button.dart';
+import 'package:relax_splash/splash%20screen/components/mood_diary_vew.dart';
+import 'package:relax_splash/splash%20screen/components/relax_view.dart';
+import 'package:relax_splash/splash%20screen/components/splash_view.dart';
+import 'package:relax_splash/splash%20screen/components/top_back_skip_view.dart';
+import 'package:relax_splash/splash%20screen/components/welcome_view.dart';
+import 'package:flutter/material.dart';
+
+class IntroductionAnimationScreen extends StatefulWidget {
+  const IntroductionAnimationScreen({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _IntroductionAnimationScreenState createState() =>
+      _IntroductionAnimationScreenState();
+}
+
+class _IntroductionAnimationScreenState
+    extends State<IntroductionAnimationScreen> with TickerProviderStateMixin {
+  AnimationController? _animationController;
+
+  @override
+  void initState() {
+    _animationController =
+        AnimationController(vsync: this, duration: const Duration(seconds: 8));
+    _animationController?.animateTo(0.0);
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _animationController?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print(_animationController?.value);
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: ClipRect(
+        child: Stack(
+          children: [
+            SplashView(
+              animationController: _animationController!,
+            ),
+            RelaxView(
+              animationController: _animationController!,
+            ),
+            CareView(
+              animationController: _animationController!,
+            ),
+            MoodDiaryVew(
+              animationController: _animationController!,
+            ),
+            WelcomeView(
+              animationController: _animationController!,
+            ),
+            TopBackSkipView(
+              onBackClick: _onBackClick,
+              onSkipClick: _onSkipClick,
+              animationController: _animationController!,
+            ),
+            CenterNextButton(
+              animationController: _animationController!,
+              onNextClick: _onNextClick,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  void _onSkipClick() {
+    _animationController?.animateTo(0.8,
+        duration: const Duration(milliseconds: 1200));
+  }
+
+  void _onBackClick() {
+    if (_animationController!.value >= 0 &&
+        _animationController!.value <= 0.2) {
+      _animationController?.animateTo(0.0);
+    } else if (_animationController!.value > 0.2 &&
+        _animationController!.value <= 0.4) {
+      _animationController?.animateTo(0.2);
+    } else if (_animationController!.value > 0.4 &&
+        _animationController!.value <= 0.6) {
+      _animationController?.animateTo(0.4);
+    } else if (_animationController!.value > 0.6 &&
+        _animationController!.value <= 0.8) {
+      _animationController?.animateTo(0.6);
+    } else if (_animationController!.value > 0.8 &&
+        _animationController!.value <= 1.0) {
+      _animationController?.animateTo(0.8);
+    }
+  }
+
+  void _onNextClick() {
+    if (_animationController!.value >= 0 &&
+        _animationController!.value <= 0.2) {
+      _animationController?.animateTo(0.4);
+    } else if (_animationController!.value > 0.2 &&
+        _animationController!.value <= 0.4) {
+      _animationController?.animateTo(0.6);
+    } else if (_animationController!.value > 0.4 &&
+        _animationController!.value <= 0.6) {
+      _animationController?.animateTo(0.8);
+    } else if (_animationController!.value > 0.6 &&
+        _animationController!.value <= 0.8) {
+      _signUpClick();
+    }
+  }
+
+  void _signUpClick() {
+    Navigator.pop(context);
+  }
+}
